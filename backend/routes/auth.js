858a16404a2d8f9845dfb7e98734bc3fcea1d03b
yashchhikara5 <yashchhikara5@gmail.com>
2022-09-23 20:41:24 +0530

    vid 51

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,34 +4,35 @@ const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const fetchuser = require('../middleware/fetchuser')
 
 const JWT_SECRET = "Harryisagoodboy";
 
-//Create a User using: POST "/api/auth/createuser". Doesn't require Auth
-router.post('/createuser',[
+//ROUTE 1: Create a User using: POST "/api/auth/createuser". Doesn't require Auth
+router.post('/createuser', [
     //Giving restrictions using express validator
     body('name', 'Enter a valid Name').isLength({ min: 3 }),
     body('email', 'Enter a valid Email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-], async (req, res) =>{//making the function asynchronous
+], async (req, res) => {//making the function asynchronous
     // If there are errors, return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ errors: errors.array() });
     }
 
-    
+
     //Wrapping whole block in a try and catch block
     try {
-        
+
         //Check whether the user with same email exists already
-        let user = await User.findOne({email: req.body.email})
-        if(user){
-            return res.status(400).json({error: "Sorry a user with this email already exists"})
+        let user = await User.findOne({ email: req.body.email })
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email already exists" })
         }
         //Creating a user using the endpoint createuser and schema User
         const salt = await bcrypt.genSalt(10);
-        const secPass = await bcrypt.hash(req.body.password, salt) 
+        const secPass = await bcrypt.hash(req.body.password, salt)
 
         user = await User.create({
             name: req.body.name,
@@ -40,7 +41,7 @@ router.post('/createuser',[
         })
 
         const data = {
-            user:{
+            user: {
                 id: user.id
             }
         }
@@ -48,12 +49,64 @@ router.post('/createuser',[
 
 
         // res.json(user)
-        res.json({authToken})
+        res.json({ authToken })
 
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("Some error occured")
+        res.status(500).send("Internal Server error occure")
     }
 })
+
+
+
+
+//ROUTE 2: Authenticate a User using: POST "/api/auth/login". Doesn't require Auth
+router.post('/login', [
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please login with correct credentials" });
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+            return res.status(400).json({ error: "Please login with correct credentials" });
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+
+        res.json({ authToken })
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server error occured")
+    }
+})
+
+//ROUTE 3: Get logged in user details using: POST "/api/auth/getuser". Login required
+router.post('/getuser', fetchuser,  async (req, res) => {
+    try {
+        userId = req.user.id;
+        const user = await User.findById(userId).select("-password");
+        res.send(user)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server error occured")
+    }
+})
+
 
 module.exports = router
